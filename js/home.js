e60dc2174eaa804e1d73340ec30262882f26c32a
ClementRayer <clement.rayer@gmail.com>
2021-09-12 19:07:04 +0200

    Home mobile V1 (to change with content + buggy)

---
+++ js/home.js
@@ -0,0 +1,5 @@
+const disclaimerBlock = document.getElementById('disclaimer');
+const disclaimerCloser = document.getElementById('disclaimer-button');
+disclaimerCloser.addEventListener('click', ()=>{
+    disclaimerBlock.style.display = 'none';
+})
